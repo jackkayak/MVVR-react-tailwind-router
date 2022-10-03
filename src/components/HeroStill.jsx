ac69c447bc0ec72c5d702ca264304592aa2ccfdd
jackkayak <jacktkay99@gmail.com>
2022-10-03 13:55:54 -0700

    better look on macbook

--- src/components/HeroStill.jsx
+++ src/components/HeroStill.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class HeroStill extends Component {
   render() {
     return (
-      <div className='radials-bg w-full h-[50vh] absolute -z-10'>
-        <div className='max-w-[1000px] text-white mx-auto px-4 flex flex-col justify-center text-center h-[90%]'>
+      <div className='radials-bg w-full h-[70vh] absolute -z-10'>
+        <div className='max-w-[1000px] text-white mx-auto px-4 flex flex-col justify-center text-center h-[100%]'>
           <h1 className='text-6xl italic font-bold tracking-wider uppercase'>{this.props.heading}</h1>
           <h3 className='tracking-wider text-2xl text-gray-300 pt-4'>{this.props.text}</h3>
         </div>
